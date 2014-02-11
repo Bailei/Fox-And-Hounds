879d136bc298e988fb27746655ff86757de3f057
Bailei <taofeng311@gmail.com>
2014-02-10 23:58:34 -0500

    add some unit test

--- src/org/client/GameLogic.java
+++ src/org/client/GameLogic.java
@@ -1,11 +1,27 @@
 package org.client;
 
+import java.util.List;
+
+import org.client.GameApi.Operation;
 import org.client.GameApi.VerifyMove;
 import org.client.GameApi.VerifyMoveDone;
 
 public class GameLogic {
   public VerifyMoveDone verify(VerifyMove verifyMove) {
-    // TODO: I will implement this method in HW2
-    return new VerifyMoveDone();
+    try{
+    	checkMoveIsLegal(verifyMove);
+    	return new VerifyMoveDone();
+    }catch(Exception e){
+    	return new VerifyMoveDone(verifyMove.getLastMovePlayerId(), e.getMessage());
+    }
+  }
+  
+  void checkMoveIsLegal(VerifyMove verifyMove){
+	  //HW2
+  }
+  
+  List<Operation> getInitialMove(int whitePlayerId, int blackPlayerId){
+		// TODO Auto-generated method stub
+		return null;
   }
 }
--- tests/org/client/GameLogicTest.java
+++ tests/org/client/GameLogicTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static com.google.common.base.Preconditions.checkArgument;
+//import static com.google.common.base.Preconditions.checkArgument;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -28,21 +28,21 @@ import com.google.common.collect.Lists;
 
 @RunWith(JUnit4.class)
 public class GameLogicTest {
+	GameLogic gameLogic = new GameLogic();
 
-	
 	private void assertMoveOk(VerifyMove verifyMove){
-		VerifyMoveDone verifyDone = new GameLogic().verify(verifyMove);
-		assertEquals(new VerifyMoveDone(), verifyDone);
+		gameLogic.checkMoveIsLegal(verifyMove);
 	}
 	
 	private void assertHacker(VerifyMove verifyMove){
 		VerifyMoveDone verifyDone = new GameLogic().verify(verifyMove);
-		assertEquals(new VerifyMoveDone(verifyMove.getLastMovePlayerId(), "Hacker Found"), verifyDone);		
+		assertEquals(verifyMove.getLastMovePlayerId(), verifyDone.getHackerPlayerId());		
 	}
 	
-	private final int wId = 41;
-	private final int bId = 42;
-	private final String playerId = "playerId";
+	private static final String PLAYER_ID = "playerId";
+	
+	private final int wId = 1;
+	private final int bId = 2;
 	private final String turn = "turn";
 	private static final String W = "W"; // White hand
 	private static final String B = "B"; // Black hand
@@ -52,10 +52,38 @@ public class GameLogicTest {
 	private final Map<String, Object> emptyState = ImmutableMap.<String, Object>of();
 	private final Map<String, Object> nonEmptyState = ImmutableMap.<String, Object>of("k", "v");
 
+	private VerifyMove move(
+			int lastMovePlayerId, 
+			Map<String, Object> lastState, 
+			List<Operation> lastMove){
+		return new VerifyMove(wId, playersInfo, emptyState, lastState, lastMove, lastMovePlayerId);
+	}
+	
+	private List<Operation> getInitialOperation(){
+		return gameLogic.getInitialMove(wId, bId);
+	}
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitialMove() {
+		assertMoveOk(move(wId, emptyState, getInitialOperation()));
 	}
 
+	@Test
+	public void testInitialMoveByWrongPlayer(){
+		assertHacker(move(bId, emptyState, getInitialOperation()));
+	}
+	
+	@Test
+	public void testInitialMoveFromNonEmptyState(){
+		assertHacker(move(wId, nonEmptyState, getInitialOperation()));
+	}
+	
+	@Test
+	public void testInitialMoveWithExtroOperation(){
+		List<Operation> initialOperations = getInitialOperation();
+		initialOperations.add(new Set("S8", "SHEEP"));
+		assertHacker(move(wId, emptyState, getInitialOperation()));
+	}
+	
+	@Test
 }
